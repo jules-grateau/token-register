f176873528c5a451fbd41b3d7360b055f001eec5
Jules Grateau <jules.grateau@gmail.com>
2025-05-20 00:44:10 +0200

    Added Responsive CSS so the app can be used in Portrait

--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import Catalog from '../Catalog';
 import Cart from '../Cart';
 import OrderModal from '../OrderModal';
+import styles from './Register.module.css';
 
 export const Register: React.FC = () => {
   const [isOrderModalOpen, setIsOrderModalOpen] = React.useState(false);
 
   return (
-    <>
+    <div className={styles.registerContainer}>
       <Cart onClickHistory={() => setIsOrderModalOpen(true)}/>
       <Catalog />
       <OrderModal isOpen={isOrderModalOpen} onClose={() => setIsOrderModalOpen(false)}/>
-    </>
+    </div>
   )
 }
