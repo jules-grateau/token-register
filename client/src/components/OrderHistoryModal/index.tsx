95024ee42bced246457657d981e46c4d62f66d4d
Jules Grateau <jules.grateau@gmail.com>
2025-05-21 18:26:01 +0200

    Split Modal and ModalOrder component to make a reusable Modal component

---
+++ client/src/components/OrderHistoryModal/index.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styles from './OrderHistoryModal.module.css';
+import { useGetOrdersQuery, useRemoveOrderMutation } from '../../services/orders';
+import CartItem from '../Cart/CartItem';
+import Button from '../Button';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import Modal from '../Modal';
+
+interface OrderHistoryModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const OrderHistoryModal: React.FC<OrderHistoryModalProps> = ({
+  isOpen,
+  onClose
+}) => {
+  const { t } = useTranslation();
+  const ordersQuery = useGetOrdersQuery();
+  const { data: ordersData, isLoading, error } = ordersQuery;
+  const [removeOrder] = useRemoveOrderMutation();
+
+
+  const onRemoveOrder = async (id : number) => {
+    try {
+      await removeOrder(id).unwrap();
+      toast.success(t('order_removed', { id }));
+    } catch (error) {
+      toast.error(t('error_removing_order', { error: String(error) }));
+    }
+    finally {
+      ordersQuery.refetch();
+    }
+  };
+
+  return (
+    <Modal 
+        isOpen={isOpen} 
+        onClose={onClose} 
+        title={t('order_history')} 
+    >
+        {isLoading && <p>{t('loading')}</p>}
+        {error && <p>{t('error_loading_orders')}</p>}
+        {ordersData && ordersData.length > 0 ? (
+        <>
+            <ul className={styles.orderList}>
+                {ordersData.map((order, index) => 
+                {
+                var totalPrice = order.items.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+                var totalItems = order.items.reduce((acc, item) => acc + item.quantity, 0)
+                return <div className={styles.order} key={index}>
+                    <li key={index}>
+                    <div className={styles.orderHeader}>
+                        <h3 className={styles.orderNumber}>{t('order_number', { id: order.id, date: new Date(order.date).toLocaleString() })}</h3>
+                        <Button
+                        onClick={() => onRemoveOrder(order.id)}
+                        color="danger"
+                        > {t('delete')} </Button>
+                    </div>
+                        <ul className={styles.orderItems}>
+                            {order.items.map((item, itemIndex) => (
+                            <CartItem
+                                key={itemIndex}
+                                item={item}
+                            />
+                            ))}
+                        </ul>
+                    </li>
+                <div className={styles.orderTotal}>
+                    <span>{totalPrice} {t('tokens', { count:totalPrice })}</span>
+                    <span>{totalItems} {t('items', { count:totalItems })}</span>
+                </div>
+                </div>
+                })}
+            </ul>
+        </>
+        ) : (
+        <p>{t('no_orders')}</p>
+        )}
+    </Modal>
+  );
+};
+
+export default OrderHistoryModal;
--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Catalog from '../Catalog';
 import Cart from '../Cart';
-import OrderModal from '../OrderModal';
+import OrderHistoryModal from '../OrderHistoryModal';
 import styles from './Register.module.css';
 
 export const Register: React.FC = () => {
@@ -12,7 +12,7 @@ export const Register: React.FC = () => {
     <div className={styles.registerContainer}>
       <Cart onClickHistory={() => setIsOrderModalOpen(true)}/>
       <Catalog />
-      <OrderModal isOpen={isOrderModalOpen} onClose={() => setIsOrderModalOpen(false)}/>
+      <OrderHistoryModal isOpen={isOrderModalOpen} onClose={() => setIsOrderModalOpen(false)}/>
     </div>
   )
 }
